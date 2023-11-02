b8bc2dab6adf15820af49b2a47c80c8a72470a59
Robert McArthur <robert.mcarthur@anu.edu.au>
2023-11-02 13:19:30 +1100

    Ultrametricity check now uses math.isclose instead

--- src/scs_analysis/data_generation/branch_length.py
+++ src/scs_analysis/data_generation/branch_length.py
@@ -1,10 +1,20 @@
 from cogent3.core.tree import PhyloNode
+import math
 
 
 def check_ultrametric(tree: PhyloNode) -> bool:
+    """
+    Given a phylogenetic tree, check whether it is ultrametric.
+
+    Args:
+        tree (PhyloNode): A phylogenetic tree
+
+    Returns:
+        bool: Whether the tree is ultrametric
+    """
     all_tips = tree.tips()
     ultrametric_length = all_tips[0].distance(tree)
     for i in range(1, len(all_tips)):
-        if all_tips[i].distance(tree) != ultrametric_length:
+        if not math.isclose(all_tips[i].distance(tree), ultrametric_length):  # type: ignore
             return False
     return True
