53bc1c8d480aa74847231554981bbff0c77978b5
rmcar17 <robert.mcarthur@anu.edu.au>
2023-11-21 23:55:28 +1100

    Update graph generation

--- src/scs_analysis/experiment/graph.py
+++ src/scs_analysis/experiment/graph.py
@@ -20,25 +20,26 @@ def graph_experiment(image_folder: str, root: str, distance_files: List[str]):
     distance_file_paths = [root + "/" + file for file in distance_files]
 
     header = [
-        "model",
-        "source",
-        "walltime",
-        "cputime",
-        "rf",
-        "mc",
-        "f1",
-        "brf",
-        "bmc",
-        "bf1",
-        "tree",
+        "Model Tree",
+        "Source Tree",
+        "Wall Time",
+        "CPU Time",
+        "RF Distance",
+        "Matching Cluster Distance",
+        "F1 Score",
+        "Bifurcating RF Distance",
+        "Bifurcating Matching Cluster Distance",
+        "Bifurcating F1 Score",
+        "Tree",
     ]
+    drops = ["Model Tree", "Source Tree", "Tree"]
     dfs = []
     for method, distance_file in zip(methods, distance_file_paths):
         if method not in METHOD_MAP:
             continue
 
         df = pd.read_csv(distance_file, delimiter="\t", names=header)
-        df = df.drop(columns=["model", "source", "tree"])
+        df = df.drop(columns=drops)
         df["method"] = METHOD_MAP[method]
         dfs.append(df)
     df = pd.concat(dfs, ignore_index=True)
@@ -48,12 +49,63 @@ def graph_experiment(image_folder: str, root: str, distance_files: List[str]):
     if not os.path.exists(image_directory):
         os.makedirs(image_directory)
 
-    for col in set(header).difference(["model", "source", "tree"]):
+    for col in set(header).difference(drops):
         plt.figure()
-        sns.boxplot(df, x="method", y=col)
+        if "Time" in col:
+            scaled, unit = make_time_scale_reasonable(df, col)
+            sns.boxplot(scaled, x="method", y=col)
+            plt.ylabel(col + f" ({unit})")
+        else:
+            sns.boxplot(df, x="method", y=col)
+        if "F1" not in col:
+            plt.ylim(0, None)
         plt.title(root)
-        plt.savefig(image_directory + f"{col}.png")
+        plt.tight_layout()
+        plt.savefig(image_directory + f"{col}_all.png")
         plt.close()
+
+        mcs_mask = df["method"] == METHOD_MAP[MCS]
+
+        mcs_data = df[mcs_mask]
+        plt.figure()
+        if "Time" in col:
+            scaled, unit = make_time_scale_reasonable(mcs_data, col)
+            sns.boxplot(scaled, x="method", y=col)
+            plt.ylabel(col + f" ({unit})")
+        else:
+            sns.boxplot(mcs_data, x="method", y=col)
+        if "F1" not in col:
+            plt.ylim(0, None)
+        plt.title(root)
+        plt.tight_layout()
+        plt.savefig(image_directory + f"{col}_mcs.png")
+        plt.close()
+
+        no_mcs_data = df[~mcs_mask & (df["method"] != METHOD_MAP[BCDN])]
+        plt.figure()
+        if "Time" in col:
+            scaled, unit = make_time_scale_reasonable(no_mcs_data, col)
+            sns.boxplot(scaled, x="method", y=col)
+            plt.ylabel(col + f" ({unit})")
+        else:
+            sns.boxplot(no_mcs_data, x="method", y=col)
+        if "F1" not in col:
+            plt.ylim(0, None)
+        plt.title(root)
+        plt.tight_layout()
+        plt.savefig(image_directory + f"{col}_others.png")
+        plt.close()
+
+
+def make_time_scale_reasonable(df, column):
+    df = df.copy()
+    if df[column].max() < 600:
+        return df, "seconds"
+    df[column] /= 60
+    if df[column].max() < 600:
+        return df, "minutes"
+    df[column] /= 60
+    return df, "hours"
 
 
 def graph_results(image_folder: str, results_folder: str):
