7458beb70bdfc319b0bd775d09a745cc2e86c7c6
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-05-03 16:45:55 +1000

    fix version format

--- src/scs_analysis/__init__.py
+++ src/scs_analysis/__init__.py
@@ -3,11 +3,7 @@ SCS Analysis
 
 An analysis repository for supertree methods.
 """
-__author__ = "Robert McArthur"
+
 __copyright__ = "Copyright 2023, Robert McArthur"
-__credits__ = ["Robert McArthur"]
 __license__ = "BSD"
-__version__ = "2023.11.06"  # A DATE BASED VERSION
-__maintainer__ = "Robert McArthur"
-__email__ = "robert.mcarthur@anu.edu.au"
-__status__ = "alpha"
+__version__ = "2023.11.6"
